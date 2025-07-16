6858be23b2a7d9992a7e93a51ab756bca8ac5a39
jasus7 <jasus7@cocoaworx.co.kr>
2025-07-16 17:27:44 +0900

    완전수정

--- src/Counter.js
+++ src/Counter.js
@@ -28,7 +28,7 @@ function Counter() {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>✏️ 입력한 내용을 수정해봤어05:</h2>
+      <h2>✏️ 입력내용을 완전히 수정해보자:</h2>
       <input
         type="text"
         value={text}
